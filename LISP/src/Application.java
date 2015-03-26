6ff995247ba08e985df1bce561fa9a95dadb37fb
ghamm <misa7403@hotmail.fr>
2015-03-26 12:05:59 +0100

    first commit

---
+++ LISP/src/Application.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String chaine="";
+		String fichier = "test.txt";//JOptionPane.showInputDialog("nom du fichier");
+
+		//lecture du fichier texte
+		try{
+			InputStream ips=new FileInputStream(fichier);
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				chaine+=ligne+"\n";
+			}
+			br.close();
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+
+		System.out.println(chaine);
+
+	}
+
+}
