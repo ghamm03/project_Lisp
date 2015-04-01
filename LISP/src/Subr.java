12416cf468783df4d9d41be68eeae63e31c0c6a5
christopher <christopher@Chris>
2015-04-01 15:34:05 +0200

    Mise à jour : ajout classe abstraite Subr.

---
+++ LISP/src/Subr.java
@@ -0,0 +1,24 @@
+
+public abstract class Subr {
+
+	public Sexpr car(Sexpr a) {
+		return null;
+	}
+
+	public Sexpr cdr(Sexpr a) {
+		return null;
+	}
+
+	public Sexpr cons(Sexpr a, Sexpr a1) {
+		return null;
+	}
+
+	public Sexpr eq(Sexpr a, Sexpr a1) {
+		return null;
+	}
+
+	public Sexpr atom(Sexpr a, Sexpr a1) {
+		return null;
+	}
+
+}
