5cdfc7e526fc25df51f7c07ea21421973baefaa1
ghamm <misa7403@hotmail.fr>
2015-04-01 13:35:00 +0200

    Classes

---
+++ LISP/src/Atome.java
@@ -0,0 +1,22 @@
+
+public class Atome {
+	
+	private String atome;
+	
+	public Atome(String mot){
+		this.setAtome(mot);
+	}
+
+	
+	
+	
+	//ACCESSEURS
+	public String getAtome() {
+		return atome;
+	}
+
+	public void setAtome(String atome) {
+		this.atome = atome;
+	}
+
+}
